98d23f281d9ef0b06adfd4c413af989814c4afa3
GoodTimeclubs <63758794+GoodTimeclubs@users.noreply.github.com>
2025-10-16 00:36:10 +0200

    Remove unused function and update quantile calculation
    Deleted the unused checkvel function from both Ex01_Intro_to_R.Rmd and a9.R. Updated a9.R to create a data frame from mtcars and clarified the use of sapply and apply for mean and quantile calculations.

--- ü1/a9.R
+++ ü1/a9.R
@@ -2,11 +2,6 @@ sapply(mtcars, mean)
 #eine Funktion auf jedes Element einer Liste, eines Vektors oder eines Datenframes anwenden und als Ergebnis einen Vektor anstelle einer Liste erhalten möchten.
 
 
-checkvel <- function(x){
-  tf <- sum(x == 0.25)
-  sf <- sum(x == 0.75)
-
-  return(c(tf,sf))
-}
-
 apply(mtcars, 2, quantile, probs = c(0.25, 0.75))
+
+df <- data.frame(mtcars)
