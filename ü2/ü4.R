864490ad976d4268da3ed0123a5ee4bda3c083f7
GoodTimeclubs <63758794+GoodTimeclubs@users.noreply.github.com>
2025-10-28 21:39:56 +0100

    ü5

--- ü2/ü4.R
+++ ü2/ü4.R
@@ -6,13 +6,22 @@
 deck <- 52
 cards <- 5
 
+# (a) Vier Asse und der Pik-König
 a_part <- choose(deck,cards)
 a_prop <- 1/a_part
 
+# (b) Nur Pik-Karten (alle 5 Karten aus 13 Pik-Karten)
 amm_spakes <- 52 / 4
 b_gueldische_decks <- floor(amm_spakes / 5)
 b_prop = b_gueldische_decks / deck
 
-c_gueldische_decks = #Anzahl decks ohne paare = gegenteil von Decks mit paaren
-
+# (c) Keine Paare (alle Werte distinct)
+c_gueldische_decks =  choose(13, 5) * 4^5  # 5 Werte aus 13, jede in 4 Farben
 c_prop = c_gueldische_decks / deck
+
+# (d) Full House (drei Karten eines Werts, zwei eines anderen)
+d_favorable <- choose(13, 1) * choose(4, 3) * choose(12, 1) * choose(4, 2)
+d_prob <- d_favorable / total_hands
+
+# Ergebnisse anzeigen
+list(a = a_prob, b = b_prob, c = c_prob, d = d_prob)
